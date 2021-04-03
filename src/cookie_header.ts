a0586a32574679b22c754478ef672bb46cdc7a16
Samir Talwar <samir@noodlesandwich.com>
2021-04-03 23:51:55 +0200

    Parse a complex Set-Cookie header.

--- src/cookie_header.ts
+++ src/cookie_header.ts
@@ -1,15 +1,64 @@
 interface SetCookieHeader {
   name: string;
   value: string;
+  expires?: Date;
+  maxAge?: number;
+  domain?: string;
+  path?: string;
+  secure?: boolean;
+  httpOnly?: boolean;
+  sameSite?: string;
 }
 
 export const parseSetCookieHeader = (
-  header: string,
+  headerString: string,
 ): SetCookieHeader | undefined => {
-  const nameValuePair = header.split("=", 2);
-  const [name, value] = nameValuePair;
+  const [firstSection, ...sections] = headerString.split(/;\s*/);
+  if (!firstSection) {
+    return;
+  }
+  const [name, value] = firstSection.split("=", 2);
   if (!name || !value) {
     return;
   }
-  return {name, value};
+  const unquotedValue =
+    value[0] === '"' && value[value.length - 1] === '"'
+      ? value.substring(1, value.length - 1)
+      : value;
+  const header: SetCookieHeader = {name, value: unquotedValue};
+  for (const section of sections) {
+    const [sectionName, sectionValue] = section.split("=", 2);
+    if (!sectionName) {
+      continue;
+    }
+    switch (sectionName.toLowerCase()) {
+      case "secure":
+        header.secure = true;
+        break;
+      case "httponly":
+        header.httpOnly = true;
+        break;
+    }
+    if (!sectionValue) {
+      continue;
+    }
+    switch (sectionName.toLowerCase()) {
+      case "expires":
+        header.expires = new Date(sectionValue);
+        break;
+      case "max-age":
+        header.maxAge = parseInt(sectionValue);
+        break;
+      case "domain":
+        header.domain = sectionValue;
+        break;
+      case "path":
+        header.path = sectionValue;
+        break;
+      case "samesite":
+        header.sameSite = sectionValue.toLowerCase();
+        break;
+    }
+  }
+  return header;
 };
--- test/cookie_header_test.ts
+++ test/cookie_header_test.ts
@@ -7,3 +7,20 @@ test("parse a simple header", (t) => {
   const parsedHeader = parseSetCookieHeader(header);
   t.deepEqual(parsedHeader, {name: "name", value: "value"});
 });
+
+test("parse a complex header", (t) => {
+  const header =
+    'name="this is a long value!"; Expires=Wed, 21 Oct 2015 07:28:00 GMT; Max-Age=2592000; Domain=example.com; Path=/dir; Secure; HttpOnly; SameSite=Strict';
+  const parsedHeader = parseSetCookieHeader(header);
+  t.deepEqual(parsedHeader, {
+    name: "name",
+    value: "this is a long value!",
+    expires: new Date("Wed, 21 Oct 2015 07:28:00 GMT"),
+    maxAge: 2592000,
+    domain: "example.com",
+    path: "/dir",
+    secure: true,
+    httpOnly: true,
+    sameSite: "strict",
+  });
+});
