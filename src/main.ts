73bc77dff0e3d5250f5542f3f9950cfcd431f3d7
Samir Talwar <samir@noodlesandwich.com>
2021-04-08 22:34:14 +0200

    Parse cookies before logging them.

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,42 @@
 import {Browser, WebRequest} from "webextension-polyfill-ts";
 
 declare const browser: Browser;
-declare const console: {log: (...items: any) => void};
 
-const logCookieHeaders = (details: WebRequest.OnHeadersReceivedDetailsType) => {
-  const {url, responseHeaders} = details;
-  const cookieHeaders = (responseHeaders || []).filter(
-    (header) => header.name.toLowerCase() == "set-cookie",
+(async () => {
+  const {SetCookieHeader} = await import("./cookie_header");
+  const logCookieHeaders = (
+    details: WebRequest.OnHeadersReceivedDetailsType,
+  ) => {
+    const {url, responseHeaders} = details;
+    const cookieHeaders = (responseHeaders || [])
+      .filter((header) => header.name.toLowerCase() == "set-cookie")
+      .flatMap((header) => {
+        if (header.value) {
+          const parsedHeader = SetCookieHeader.parse(header.value);
+          if (parsedHeader) {
+            return [parsedHeader];
+          } else {
+            return [];
+          }
+        } else {
+          return [];
+        }
+      })
+      .map((header) => ({
+        name: header.name(),
+        value: header.value(),
+        expires: header.expires(),
+        maxAge: header.maxAge(),
+      }));
+    if (cookieHeaders.length > 0) {
+      console.log({url, cookieHeaders});
+    }
+    return {};
+  };
+
+  browser.webRequest.onHeadersReceived.addListener(
+    logCookieHeaders,
+    {urls: ["<all_urls>"]},
+    ["blocking", "responseHeaders"],
   );
-  if (cookieHeaders.length > 0) {
-    console.log({url, cookieHeaders});
-  }
-  return {};
-};
-
-browser.webRequest.onHeadersReceived.addListener(
-  logCookieHeaders,
-  {urls: ["<all_urls>"]},
-  ["blocking", "responseHeaders"],
-);
+})().catch(console.error);
