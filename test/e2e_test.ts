48bc76d37d357492fb2127ce4d37c6bba7b579cd
Samir Talwar <samir@noodlesandwich.com>
2021-04-18 15:44:23 +0200

    Test that cookie expiry times are capped at a week.

--- test/e2e_test.ts
+++ test/e2e_test.ts
@@ -10,6 +10,12 @@ import {
 
 import newCookieServer from "./cookie-server/server";
 
+const ONE_MINUTE = 60;
+const ONE_HOUR = ONE_MINUTE * 60;
+const ONE_DAY = ONE_HOUR * 24;
+const ONE_WEEK = ONE_DAY * 7;
+const TWO_WEEKS = ONE_WEEK * 2;
+
 const root = __dirname + "/../";
 
 let cookieServerUrl: string;
@@ -70,34 +76,79 @@ afterEach(async () => {
   await Promise.all([stopFirefox(), stopCookieServer()]);
 });
 
-test("cookies are set as usual", async () => {
-  const now = Date.now() / 1000;
-  await driver.navigate().to(cookieServerUrl);
-  await driver
-    .findElement(By.name("new-cookie"))
-    .sendKeys("some_name=the cookie value; Max-Age=3600; SameSite=Strict");
+const submitNewCookie = async (
+  name: string,
+  setCookieHeader: string,
+): Promise<void> => {
+  await driver.findElement(By.name("new-cookie")).sendKeys(setCookieHeader);
   await driver
     .actions()
     .click(driver.findElement(By.id("submit-new-cookie")))
     .perform();
   await driver.wait(async () => {
-    const cookieListItems = await driver.findElements(By.css("#cookies li"));
-    return cookieListItems.length > 0;
+    const cookieNameElements = await driver.findElements(
+      By.css("#cookies .cookie-name"),
+    );
+    const cookieNames = await Promise.all(
+      cookieNameElements.map((element) => element.getText()),
+    );
+    return cookieNames.indexOf(name) >= 0;
   });
+};
 
+const readCookies = async (): Promise<Array<{name: string; value: string}>> => {
   const cookieListItems = await driver.findElements(By.css("#cookies li"));
-  const serverCookies = await Promise.all(
-    cookieListItems.map((item) =>
-      Promise.all([
+  return Promise.all(
+    cookieListItems.map(async (item) => {
+      const [name, value] = await Promise.all([
         item.findElement(By.css(".cookie-name")).getText(),
         item.findElement(By.css(".cookie-value")).getText(),
-      ]),
-    ),
+      ]);
+      return {name, value};
+    }),
   );
-  expect(serverCookies).toStrictEqual([["some_name", "the cookie value"]]);
+};
+
+test("cookies are set as usual", async () => {
+  const now = Date.now() / 1000;
+  await driver.navigate().to(cookieServerUrl);
+  await submitNewCookie(
+    "some_name",
+    `some_name=the cookie value; Max-Age=${ONE_HOUR}; SameSite=Strict`,
+  );
+
+  const serverCookies = await readCookies();
+  expect(serverCookies).toStrictEqual([
+    {
+      name: "some_name",
+      value: "the cookie value",
+    },
+  ]);
 
   const cookie = await driver.manage().getCookie("some_name");
   expect(cookie.value).toBe("the cookie value");
-  expect(cookie.expiry).toBeGreaterThan(now + 3600 - 5);
-  expect(cookie.expiry).toBeLessThan(now + 3600 + 5);
+  expect(cookie.expiry).toBeGreaterThan(now + ONE_HOUR - 5);
+  expect(cookie.expiry).toBeLessThan(now + ONE_HOUR + 5);
+});
+
+test("long-lived cookies are capped at a week", async () => {
+  const now = Date.now() / 1000;
+  await driver.navigate().to(cookieServerUrl);
+  await submitNewCookie(
+    "long_lived",
+    `long_lived=some random value; Max-Age=${TWO_WEEKS}; SameSite=Strict`,
+  );
+
+  const serverCookies = await readCookies();
+  expect(serverCookies).toStrictEqual([
+    {
+      name: "long_lived",
+      value: "some random value",
+    },
+  ]);
+
+  const cookie = await driver.manage().getCookie("long_lived");
+  expect(cookie.value).toBe("some random value");
+  expect(cookie.expiry).toBeGreaterThan(now + ONE_WEEK - 5);
+  expect(cookie.expiry).toBeLessThan(now + ONE_WEEK + 5);
 });
