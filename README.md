8edd88624af24cf0ea85c5e7f14d4c7771d84516
Samir Talwar <samir@noodlesandwich.com>
2021-04-25 20:38:41 +0200

    Provide a button to limit pre-existing cookies.

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A browser extension that persuades your browser to forget information after a wh
 - [x] Limit cookies to a week.
 - [x] Make the maximum cookie expiry time customizable.
 - [x] Prompt the user to configure the extension on installation.
-- [ ] Provide a button to overwrite cookie expiry times for pre-existing cookies.
+- [x] Provide a button to overwrite cookie expiry times for pre-existing cookies.
 - [ ] Optionally overwrite cookie expiry times for pre-existing cookies on startup.
 - [ ] Optionally wipe session cookies on startup.
 - [ ] Customize the maximum cookie expiry time per domain.
--- src/options_page.ts
+++ src/options_page.ts
@@ -1,10 +1,61 @@
+const errorBox = document.getElementById("error")!;
+const errorMessageElement = document.getElementById("error-message")!;
+const dismissErrorButton = document.getElementById("error-dismiss")!;
+
+const limitSelector = document.getElementById("option-limit")!;
+const limitAllCookies = document.getElementById("limit-all-cookies")!;
+
+const reportError = (error: any): void => {
+  errorBox.style.display = "inherit";
+  if (error instanceof Error) {
+    errorMessageElement.textContent = `Error: ${error.message}`;
+  } else {
+    errorMessageElement.textContent = `Error: ${error}`;
+  }
+};
+
 (async () => {
+  const {browser} = await import("webextension-polyfill-ts");
   const options = await import("./options");
 
-  const limitSelector = document.querySelector("[name=limit]")!;
+  const currentOptions = await options.get();
+
+  const refreshPage = async () => {
+    const violatingCookiesSection = document.getElementById(
+      "violating-cookies",
+    )!;
+    const violatingCookiesDescription = document.getElementById(
+      "violating-cookies-description",
+    )!;
+    if (currentOptions.cookieLimitInSeconds) {
+      const now = Date.now() / 1000;
+      const limit = now + currentOptions.cookieLimitInSeconds;
+      const cookies = await browser.cookies.getAll({});
+      const cookiesInViolation = cookies.filter(
+        (cookie) =>
+          cookie.expirationDate != null && cookie.expirationDate > limit,
+      );
+      const cookiesInViolationCount = cookiesInViolation.length;
+      if (cookiesInViolationCount > 0) {
+        violatingCookiesSection.style.display = "inherit";
+        if (cookiesInViolationCount === 1) {
+          violatingCookiesDescription.textContent =
+            "There is 1 cookie that violates the above limit.";
+        } else {
+          violatingCookiesDescription.textContent = `There are ${cookiesInViolationCount} cookies that violate the above limit.`;
+        }
+      } else {
+        violatingCookiesSection.style.display = "";
+      }
+    } else {
+      violatingCookiesSection.style.display = "";
+    }
+  };
 
   await (async () => {
-    const currentOptions = await options.get();
+    dismissErrorButton.addEventListener("click", () => {
+      errorBox.style.display = "";
+    });
 
     var selected = false;
     for (const {description, value} of options.COOKIE_LIMIT_OPTIONS) {
@@ -27,20 +78,52 @@
       foreverOptionElement.selected = true;
     }
     limitSelector.appendChild(foreverOptionElement);
+
+    limitSelector.addEventListener("change", async (event) => {
+      const cookieLimitValue = (event.target as HTMLSelectElement).value;
+      currentOptions.cookieLimitInSeconds =
+        cookieLimitValue.length > 0 ? parseInt(cookieLimitValue) : undefined;
+      await options.set(currentOptions);
+      await refreshPage();
+    });
+
+    limitAllCookies.addEventListener("click", async () => {
+      try {
+        if (currentOptions.cookieLimitInSeconds) {
+          const now = Date.now() / 1000;
+          const limit = now + currentOptions.cookieLimitInSeconds;
+          const cookies = await browser.cookies.getAll({});
+          await Promise.all(
+            cookies
+              .filter(
+                (cookie) =>
+                  cookie.expirationDate != null &&
+                  cookie.expirationDate > limit,
+              )
+              .map((cookie) => ({
+                domain: cookie.domain,
+                firstPartyDomain: cookie.firstPartyDomain,
+                httpOnly: cookie.httpOnly,
+                name: cookie.name,
+                path: cookie.path,
+                sameSite: cookie.sameSite,
+                secure: cookie.secure,
+                storeId: cookie.storeId,
+                value: cookie.value,
+                expirationDate: limit,
+                url:
+                  (cookie as any).url ||
+                  cookie.domain.replace(/^(\.)?/, "https://") + cookie.path,
+              }))
+              .map((cookie) => browser.cookies.set(cookie)),
+          );
+          await refreshPage();
+        }
+      } catch (error) {
+        reportError(error);
+      }
+    });
   })();
 
-  limitSelector.addEventListener("change", (event) => {
-    const cookieLimitValue = (event.target as HTMLSelectElement).value;
-    const cookieLimitInSeconds =
-      cookieLimitValue.length > 0 ? parseInt(cookieLimitValue) : undefined;
-    options.set({
-      cookieLimitInSeconds,
-    });
-  });
-})().catch((error) => {
-  if (error instanceof Error) {
-    document.body.textContent = `Error: ${error.message}`;
-  } else {
-    document.body.textContent = `Error: ${error}`;
-  }
-});
+  await refreshPage();
+})().catch(reportError);
